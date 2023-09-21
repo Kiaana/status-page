38c6446f47247d8133466cbc9905c23a0ebe29f4
Kiaana <kiaana@163.com>
2023-09-21 23:22:03 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm795334055-95707a269327bcc32849522c',
+    'm795335916-e2a0a8ba9df02b302b28200f',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Kiaana\'s Blog',
+      url: 'https://www.kiaana.cn/'
+    },
+    {
+      text: 'Kiaana网盘',
+      url: 'https://pan.kiana.run/'
+    },
+    {
+      text: '联系站长',
+      url: 'mailto:contact@kiaana.cn'
+    },
+  ],
+};
